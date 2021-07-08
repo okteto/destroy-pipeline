4173ba8af5d6eff83c10a9e30f574855c8377f4f
Javier López Barba <javier@okteto.com>
2021-07-08 13:11:32 +0200

    Add support for custom certificates
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,11 @@ if [ ! -z $namespace ]; then
 params="--namespace=$namespace"
 fi
 
+if [[ ! -z "$OKTETO_CA_CERT" ]]; then
+   echo "Custom certificate is provided"
+   echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert
+   update-ca-certificates
+fi
+
 echo running: okteto pipeline destroy --name "${name}" ${params} --wait
 okteto pipeline destroy --name "${name}" ${params} --wait
