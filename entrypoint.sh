a7efcdf9de88cc9465f009bbdc56eaedf1266ce2
Teresa Romero <teresa@okteto.com>
2024-05-24 16:07:15 +0200

    added log level
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 name=$1
 namespace=$2
+log_level=$3
 
 params=""
 if [ ! -z $namespace ]; then
@@ -15,5 +16,21 @@ if [ ! -z "$OKTETO_CA_CERT" ]; then
    update-ca-certificates
 fi
 
-echo running: okteto pipeline destroy --name "${name}" ${params} --wait
-okteto pipeline destroy --name "${name}" ${params} --wait
+if [ ! -z "$log_level" ]; then
+  if [ "$log_level" = "debug" ] || [ "$log_level" = "info" ] || [ "$log_level" = "warn" ] || [ "$log_level" = "error" ] ; then
+    log_level="--log-level ${log_level}"
+  else
+    echo "log-level supported: debug, info, warn, error"
+    exit 1
+  fi
+fi
+
+# https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+if [ "${RUNNER_DEBUG}" = "1" ]; then
+  log_level="--log-level debug"
+fi
+
+
+echo running: okteto pipeline destroy $log_level --name "${name}" ${params} --wait
+okteto pipeline destroy $log_level --name "${name}" ${params} --wait
